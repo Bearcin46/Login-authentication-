d53ff42be5fd4e3104c275fa4d5a0a16464d7364
Bearcin46 <imsweety33@gmail.com>
2024-03-29 19:42:11 +0530

    feat: connected to mongodb database

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,7 +5,15 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 const server = express();
+server.use(express.json());
+server.use(cors());
 const SERVER_PORT = 1234;
+
+const connection: string = process.env.MONGODB_URL || "";
+console.log(connection);
+mongoose.connect(connection).then(() => {
+  console.log("connected to db");
+});
 
 server.get("/", (req, res) => {
   res.send("server connected successfully");
