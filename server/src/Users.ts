c2c5d189e05145efe1ba011dc073042747067932
Bearcin46 <imsweety33@gmail.com>
2024-03-29 19:56:34 +0530

    chore: generating JWT token initiated

---
+++ server/src/Users.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+const UserSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+//export const UserModel = mongoose.model("users", UserSchema);
+
+UserSchema.methods.generateAuthToken = function () {};
