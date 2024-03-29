f868279e15e464ca29ba8217da4c7996498c914e
Bearcin46 <imsweety33@gmail.com>
2024-03-29 20:29:43 +0530

    feat: validation initiated

--- server/src/Users.ts
+++ server/src/Users.ts
@@ -1,4 +1,8 @@
 import mongoose from "mongoose";
+import * as jwt from "jsonwebtoken";
+import * as Joi from "joi";
+import * as PasswordComplexity from "joi-password-complexity";
+
 const UserSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
@@ -6,6 +10,21 @@ const UserSchema = new mongoose.Schema({
   password: { type: String, required: true },
 });
 
-//export const UserModel = mongoose.model("users", UserSchema);
+UserSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this.id }, process.env.SECRET_KEY, {
+    expiresIn: "7d",
+  });
+  return token;
+};
 
-UserSchema.methods.generateAuthToken = function () {};
+export const UserModel = mongoose.model("users", UserSchema);
+
+const validate = (data) => {
+  const schema = Joi.object({
+    firstName: Joi.string().required().label("First Name"),
+    lastName: Joi.string().required().label("Last Name"),
+    email: Joi.string().email().required().label("Email"),
+    password: PasswordComplexity().required().label("Password"),
+  });
+  return schema.validate(data);
+};
