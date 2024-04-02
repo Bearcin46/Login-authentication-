86e704e5db027a1d86c49ac7b9897a26cbd4a2e7
Bearcin46 <imsweety33@gmail.com>
2024-04-02 16:58:26 +0530

    fix: unwanted nodemodules deleted

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
-
+// import { useState } from "react";
+import Signup from "./Components/Signup";
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <>
       <h1 className="bg-green-600">hello</h1>
+      <Signup />
     </>
   );
 }
---
+++ client/src/Components/Signup.tsx
@@ -0,0 +1,11 @@
+import { useForm } from "react-hook-form";
+
+const Signup = () => {
+  return (
+    <div>
+      <h1>Hi all im signup</h1>
+    </div>
+  );
+};
+
+export default Signup;
